42a454073dbc04a0613f2d79d7c93ad0f490d48c
Kuick An Kiat Tyler <131850519+TylerKuick@users.noreply.github.com>
2024-03-30 15:02:15 +0800

    amplify folder

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "indsinghealthenhancead19bd76": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "identifyText81adced4": {
+      "format": "string",
+      "region": "string"
+    }
+  }
+}
